1678cab499af34c1b07d00bcbe5be19c0ad0d0b8
Okechukwu <okigbookey@gmail.com>
2018-02-11 05:56:04 -0500

    Docs: Update comments on app.py
     Changes to be committed:
    	modified:   app.py

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ class Patient(Resource):
         return {'patient': name}
 
 
-# to get info we use the below
-# just like calling: http://127.0.0.1:5000/student/Rolf
-# not use of @app.route again
+# to get info we use add_resource method below
+# just like calling: http://127.0.0.1:5000/patient/Peter
+# without the use of @app.route decorator
 api.add_resource(Patient, '/patient/<string:name>')
 
 
