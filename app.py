c7639f08ca178bad6d3e494ad1758d0c2f46a237
Okechukwu <okigbookey@gmail.com>
2018-02-11 05:41:31 -0500

    refactor: Update app.py
     Changes to be committed:
    	modified:   app.py

--- app.py
+++ app.py
@@ -9,16 +9,16 @@ app = Flask(__name__)
 api = Api(app)
 
 
-class Student(Resource):
-    """docstring for Student.Resource."""
+class Patient(Resource):
+    """docstring for Patient.Resource."""
     def get(self, name):
-        return {'student': name}
+        return {'patient': name}
 
 
 # to get info we use the below
 # just like calling: http://127.0.0.1:5000/student/Rolf
 # not use of @app.route again
-api.add_resource(Student, '/student/<string:name>')
+api.add_resource(Patient, '/patient/<string:name>')
 
 
 
