9800588e4841f188bd9bfa84fff48e7006394d2e
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-02-14 18:33:42 -0500

    feat: Implement endpoints

--- app2.py
+++ app2.py
@@ -1,49 +1,59 @@
 """This is the Python app in Virtual Environment."""
 
 
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-
-
 Patients = [
     {
         'name': 'James',
-        'Biodata':[
+        'Biodata': [
             {
-            'sex': 'male',
-            'age': 44,
-            'race': 'black'
+             'sex': 'male',
+             'age': 44,
+             'race': 'black'
             }
         ]
     },
     {
         'name': 'John',
-        'Biodata':[
+        'Biodata': [
             {
-            'sex': 'male',
-            'age': 30,
-            'race': 'White'
+             'sex': 'male',
+             'age': 30,
+             'race': 'White'
             }
         ]
     },
     {
         'name': 'Jane',
-        'Biodata':[
+        'Biodata': [
             {
-            'sex': 'female',
-            'age': 21,
-            'race': 'Latino'
+             'sex': 'female',
+             'age': 21,
+             'race': 'Latino'
             }
         ]
     }
 ]
 
 
-@app.route('/patient', methods=['POST'])
-def add_patient():
-    pass
+@app.route('/patient/<string:name>', methods=['POST'])
+def add_patient(name):
+    postdata = request.get_json()
+    new_data = {
+                    'name': name,
+                    'Biodata': [
+                        {
+                         'sex': postdata['sex'],
+                         'age': postdata['age'],
+                         'race': postdata['race']
+                        }
+                    ]
+                }
+    Patients.append(new_data)
+    return jsonify({'Patients': Patients})
+
 
 @app.route('/patient/<string:name>')
 def get_patientInfor(name):
@@ -53,9 +63,19 @@ def get_patientInfor(name):
     return 'Patient: {} not found in our patient\'s database'.format(name)
 
 
+@app.route('/patient/<string:name>/<string:Biodata>')
+def get_biodata(name, Biodata):
+    for patient in Patients:
+        if patient['name'] == name:
+            patientbios = patient['Biodata']
+            return jsonify({'Biodata': patientbios})
+    return 'Patient: {} not found in our patient\'s database'.format(name)
+
+
 @app.route('/patients')
 def get_allPatients():
     return jsonify({'All Patient': Patients})
+
 
 if __name__ == '__main__':
     app.debug = True
