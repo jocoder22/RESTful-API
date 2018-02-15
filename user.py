3631b0d32369b926ae9f641ae2b5868bbdba5acd
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-02-14 21:39:53 -0500

    feat: Add new file

---
+++ user.py
@@ -0,0 +1,18 @@
+"""
+Create a table to store user's info.
+
+User's table.
+"""
+
+
+class User(object):
+    """docstring for user.
+
+    user's table.
+    """
+
+    def __init__(self, _id, username, password):
+        """Initialize the class."""
+        self.id = user_id
+        self.username = username
+        self.password = password
