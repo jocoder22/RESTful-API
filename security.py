ceba4f5addcec27794930e66e138cbe26d69d5fe
Okigbo <okigbookey@gmail.com>
2018-02-15 12:35:51 -0500

    Refactor: Update authenticate and identity functions
     Changes to be committed:
    	modified:   security.py

--- security.py
+++ security.py
@@ -6,20 +6,22 @@ Create file for storing user info.
 from werkzeug.security import safe_str_cmp
 from user import User
 
-users = [
-    User(1, 'james', 'funny')
-]
-
-username_mapping = {u.username: u for u in users}
-userid_mapping = {u.id: u for u in users}
+# users = [
+#     User(1, 'james', 'funny')
+# ]
+#
+# username_mapping = {u.username: u for u in users}
+# userid_mapping = {u.id: u for u in users}
 
 
 def authenticate(username, password):
-    user1 = username_mapping.get(username, None)
+    # user1 = username_mapping.get(username, None)
+    user1 = User.findUser(username)
     if user1 and safe_str_cmp(user1.password, password):
         return user1
 
 
 def identity(payload):
     user_id = payload['identity']
-    return userid_mapping.get(user_id, None)
+    # return userid_mapping.get(user_id, None)
+    return User.findUserId(user_id)
